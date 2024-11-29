a0c8aba6a29c019ab5d51b34e693b1c693eb5355
fasiash <fasiuddin@codetru.com>
2024-11-29 13:07:42 +0530

    config updated to prod cred

--- src/test/java/com/Japfu/testcases/Approve_Employee_Profile_Updates_Test.java
+++ src/test/java/com/Japfu/testcases/Approve_Employee_Profile_Updates_Test.java
@@ -9,9 +9,9 @@ public class Approve_Employee_Profile_Updates_Test extends BaseTest {
 
 	@Test(priority = 1)
 	public void SC_Verify_Profile_Update_Is_Pending_Approval() {
-		getLogin().Valid_Admin_Username_Password("genrocket@myself.fr.nf", FrameworkConstants.PASSWORD);
+		getLogin().Valid_Admin_Username_Password(FrameworkConstants.USERNAME, FrameworkConstants.PASSWORD);
 		getEmployee_UpdatesPage().Approve_Employee_Personal_Details();
-		getLogout_page().Logout2();
+		getLogout_page().Logout();
 
 
 	}
